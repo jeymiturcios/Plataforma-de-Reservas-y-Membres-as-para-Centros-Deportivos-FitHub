a1b504abe4920d4b2ab059d55efa58e87ba1c009
Chris Duarte <cristoferduarte18@gmail.com>
2026-03-21 09:33:27 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 import { validarUsuario } from './middleware/validation.js';
 import { errorHandler } from './middleware/errorHandler.js';
+
+import clientesRouter from './routes/clientes.js';
+import actividadesRouter from './routes/actividades.js';
+import reservasRouter from './routes/reservas.js';
+import pagosRouter from './routes/pagos.js';
 
 const app = express();
 const PORT = 3000;
 
-// Middleware para leer JSON
+app.use(cors());
 app.use(express.json());
 
-// Ruta básica
 app.get('/', (req, res) => {
   res.send('Servidor funcionando 🚀');
 });
 
-// RUTA DE PRUEBA (POSTMAN)
 app.post('/test', (req, res) => {
   console.log(req.body);
   res.json({
@@ -22,18 +26,20 @@ app.post('/test', (req, res) => {
   });
 });
 
-//RUTA CON VALIDACIONES
 app.post('/usuarios', validarUsuario, (req, res) => {
   res.json({
-    mensaje: "Usuario válido",
+    mensaje: 'Usuario válido',
     data: req.body
   });
 });
 
-// MANEJO DE ERRORES 
+app.use('/clientes', clientesRouter);
+app.use('/actividades', actividadesRouter);
+app.use('/reservas', reservasRouter);
+app.use('/pagos', pagosRouter);
+
 app.use(errorHandler);
 
-// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
