b78c246e8bb74bd01c8f8d04078d836bb7222935
GerardoPerdomo-7 <vladimirperdomoc@gmail.com>
2026-03-27 18:44:35 -0600

    Feat: agregar sede pagos

--- routes/pagos.js
+++ routes/pagos.js
@@ -45,4 +45,47 @@ router.delete("/:id", async (req, res, next) => {
   }
 });
 
-export default router;
+
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query("SELECT * FROM pago WHERE pago_id = $1", [id]);
+
+   
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "El registro de pago no existe" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+router.get("/filtro/cliente", async (req, res, next) => {
+  try {
+    const { id_cliente } = req.query; 
+    
+    if (!id_cliente) {
+      return res.status(400).json({ error: "Falta el ID del cliente (?id_cliente=X)" });
+    }
+
+    const result = await pool.query(
+      "SELECT * FROM pago WHERE persona_id = $1 ORDER BY pago_id DESC", 
+      [id_cliente]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ 
+        error: `No se encontraron pagos asociados al cliente con ID: ${id_cliente}` 
+      });
+    }
+
+    res.json(result.rows);
+  } catch (error) {
+    next(error);
+  }
+});
+
